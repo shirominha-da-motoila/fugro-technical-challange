4ad130b94ac06684bbe71fcd95b0000a448898f8
Lukas Shiroma <lukasshiroma@gmail.com>
2024-12-20 12:55:44 -0300

    fixed warming

--- Web/ApiClient.cs
+++ Web/ApiClient.cs
@@ -4,7 +4,7 @@ namespace Web;
 
 public class ApiClient(HttpClient httpClient)
 {
-    public async Task<CalculateOffsetAndStationOutput> CalculateOffsetAndStationAsync(CalculateOffsetAndStationInput input, CancellationToken cancellationToken = default)
+    public async Task<CalculateOffsetAndStationOutput?> CalculateOffsetAndStationAsync(CalculateOffsetAndStationInput input, CancellationToken cancellationToken = default)
     {
         var response = await httpClient.PostAsJsonAsync("/CalculateOffsetAndStation", input, cancellationToken);
         var output = await response.Content.ReadFromJsonAsync<CalculateOffsetAndStationOutput>(cancellationToken);
